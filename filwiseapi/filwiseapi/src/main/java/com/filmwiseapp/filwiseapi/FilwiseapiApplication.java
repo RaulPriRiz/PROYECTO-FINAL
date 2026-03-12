7a6b92139fdf3be60fe69cb60cbfc0022b79f663
Fran <franmensajero@gmail.com>
2026-03-12 20:45:19 +0100

    M

---
+++ filwiseapi/filwiseapi/src/main/java/com/filmwiseapp/filwiseapi/FilwiseapiApplication.java
@@ -0,0 +1,13 @@
+package com.filmwiseapp.filwiseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilwiseapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilwiseapiApplication.class, args);
+	}
+
+}
---
+++ filwiseapi/filwiseapi/src/test/java/com/filmwiseapp/filwiseapi/FilwiseapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filmwiseapp.filwiseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilwiseapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
