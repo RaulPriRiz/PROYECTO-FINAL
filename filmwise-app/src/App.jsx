db7e4fe5a2858decd048c9a2066a0ac7ce4a440f
RaulPriRiz <rpririz041@g.educaand.es>
2026-04-03 19:21:53 +0200

    Subo pantalla detalles V1

--- filmwise-app/src/App.jsx
+++ filmwise-app/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Register from "./pages/Register";
 import Home from "./pages/home";
-
+import MovieDetails from "./pages/MovieDetails";
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/details" element={<MovieDetails />} />
       </Routes>
     </BrowserRouter>
   );
